18c7f80af554e612a014436db10305b17a4dc358
Rob Trehy <robert.trehy@outlook.com>
2021-05-20 10:07:02 +0100

    Initial Commit

---
+++ Plugin.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace WebApps\Plugin;
+
+use App\Models\Plugin;
+
+class Slider_Plugin extends Plugin
+{
+    public $name;
+    public $icon;
+    public $version;
+    public $author;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $plugin = json_decode(file_get_contents(__DIR__ . '/plugin.json'), true);
+        $this->name = $plugin['name'];
+        $this->icon = $plugin['icon'];
+        $this->version = $plugin['version'];
+        $this->author = $plugin['author'];
+    }
+
+    public $options = [
+        'slides' => [
+            'type' => 'repeater',
+            'label' => 'Image Slide',
+            'ref' => 'caption',
+            'options' => [
+                'imageUrl' => [
+                    'type' => 'image',
+                    'required' => true,
+                ],
+                'caption' => [
+                    'type' => 'text',
+                    'label' => 'Image Caption',
+                    'maxLength' => 255,
+                    'required' => false,
+                ]
+            ]
+        ]
+    ];
+
+    public $new = [
+        'slides' => [
+            [
+                'imageUrl' => [
+                    'src' => '',
+                    'text' => '',
+                    'class' => 'hidden',
+                    'readonly' => false,
+                    'label' => 'Get from URL:',
+                ],
+                'caption' => ''
+            ]
+        ]
+    ];
+
+    public $preview = [
+        'slides' => [
+            'each' => '<div class="slider-item" id="slide{index-1}">
+                        <img class="w-full" src="{value.imageUrl.src}" alt="{value.caption}" />
+                        <div class="absolute bottom-5 left-0 right-0 text-center">
+                            <p class="inline-block font-semibold text-sm text-white p-2 bg-gray-800 bg-opacity-60 rounded" data-val="value.caption"></p>
+                        </div>
+                       </div>'
+        ],
+        'repeater' => "$('.slider-item').removeClass('show'); $('#slide'+repeater).addClass('show');"
+    ];
+
+    public function output($edit = false)
+    {
+        $this->edit = $edit;
+        ob_start();
+        require(__DIR__.'/include/_html.php');
+        $html = str_replace(["\r", "\n", "\t"], '', trim(ob_get_clean()));
+        $html = preg_replace('/(\s){2,}/s', '', $html);
+        return $html;
+    }
+
+    public function style()
+    {
+        return file_get_contents(__DIR__.'/include/_style.css');
+    }
+
+    public function scripts($edit = false)
+    {
+        if ($edit) {
+            return '$("#slide1").addClass("show");';
+        } else {
+            return file_get_contents(__DIR__.'/include/_slider.js');
+        }
+    }
+}
---
+++ include/_html.php
@@ -0,0 +1,38 @@
+<div class="Slider" id="slider">
+
+    <?php $i = 1;
+    foreach ($this->settings['slides'] as $slide) : ?>
+        <div class="slider-item <?php echo (($i == 1) && (!$this->edit)) ? 'show' : ''; ?>" id="slide<?php echo $i; ?>">
+            <img class="w-full"
+                    src="<?php echo $slide['imageUrl']['src']; ?>"
+                    alt="<?php echo $slide['caption']; ?>"/>
+            <div class="absolute bottom-5 left-0 right-0 text-center">
+                <p class="inline-block font-semibold text-sm text-white p-2 bg-gray-800 bg-opacity-60 rounded"><?php echo $slide['caption']; ?></p>
+            </div>
+        </div>
+    <?php $i++;
+    endforeach;
+    if ($i == 1) : ?>
+        <div class="slider-item show" id="slide<?php echo $i; ?>">
+            <img class="w-full"
+                    src="https://placehold.it/1920x1080&text=Your first image will appear here"
+                    alt="Select Image First"/>
+                <div class="absolute bottom-5 left-0 right-0 text-center">
+                    <p class="inline-block font-semibold text-sm text-white p-2 bg-gray-800 bg-opacity-60 rounded"><?php echo $slide['caption']; ?></p>
+                </div>
+        </div>
+    <?php endif; ?>
+
+    <button class="prev">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+        </svg>
+    </button>
+    <button class="next">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+        </svg>
+    </button>
+    
+    <span class="switch"></span>
+</div>
